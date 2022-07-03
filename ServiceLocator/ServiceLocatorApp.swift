b35e7a6a44f04731fdbd6c013fb55d36d8009b84
Andrei <andrei.parchukov@t-systems.com>
2022-07-03 23:16:18 +0300

    Initial Commit

---
+++ ServiceLocator/ServiceLocatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ServiceLocatorApp.swift
+//  ServiceLocator
+//
+//  Created by Андрей on 03.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct ServiceLocatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
